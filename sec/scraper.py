081e974be6c2cae018815da9615b71b974ce3531
rich891216 <rich8981216@gmail.com>
2024-05-24 10:43:42 -0500

    add download csv

--- sec/scraper.py
+++ sec/scraper.py
@@ -72,9 +72,9 @@ for fund, url in URLS.items():
     reference_link = BASE_URL + quarter_data[reference_link_start:reference_link_end]
 
     # open chrome (you can use firefox or other browsers, just look up selenium.webdriver)
-    chrome_options = webdriver.ChromeOptions()
-    prefs = {'download.default_directory=~/Desktop/sec-for-noah'}
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    options = webdriver.ChromeOptions()
+    options.add_argument('download.default_directory=~/Desktop/sec-for-noah/')
+    driver = webdriver.Chrome(chrome_options=options)
     driver.get(reference_link)
 
     # wait for results
